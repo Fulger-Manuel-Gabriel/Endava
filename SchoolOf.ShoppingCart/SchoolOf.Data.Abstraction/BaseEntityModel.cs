d92d7e4fe478b79f713e8c8a2d260c70d2637cb9
Gabriel <fulgermgabriel@gmail.com>
2021-09-09 13:33:00 +0300

    09.09.2021

---
+++ SchoolOf.ShoppingCart/SchoolOf.Data.Abstraction/BaseEntityModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOf.Data.Abstraction
+{
+    class BaseEntityModel
+    {
+        public long Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public DateTimeOffset ModifiedAt { get; set; }
+    }
+}
---
+++ SchoolOf.ShoppingCart/SchoolOf.Data.Abstraction/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SchoolOf.Data.Abstraction
+{
+    public interface IUnitOfWork
+    {
+        IRepository<T> GetRepository<T>() where T : BaseEntityModel;
+
+        Task<bool> SaveChangesAsync();
+    }
+}
